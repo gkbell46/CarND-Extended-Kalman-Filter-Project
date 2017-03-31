4232b255208483bf78f8cede62b6b74ed2019739
gkbel46 <girish.bellenavar@gmail.com>
2017-03-31 23:57:03 +0530

    Corrected jacobian matrix

--- src/tools.cpp
+++ src/tools.cpp
@@ -49,6 +49,7 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+  
   MatrixXd Hj(3,4);
   //recover state parameteres
   float px = x_state(0);
@@ -61,20 +62,22 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   float p2sum = px_2 + py_2;
   float psqrt = sqrt(p2sum);
   float p3_2 = p2sum * psqrt;
-
+  
   //check division by zero
   if (fabs(p2sum) < 0.0001)
   {
     std::cout << "Denomination cannot be zero" << std::endl;
     Hj << 0, 0, 0, 0,
-         -0, 0, 0, 0,
+          0, 0, 0, 0,
           0, 0, 0, 0;
     return Hj;
   }
   Hj << (px/psqrt), (py/psqrt),0,0,
-       -(py/p2sum), (px/psqrt),0,0,
-        py*(vx*py-vy*px)/p3_2,px*(vy*px-vx*py)/p3_2,px/psqrt,py/psqrt;
-
+       -(py/p2sum), (px/p2sum),0,0,
+        py*((vx*py)-(vy*px))/p3_2,px*((vy*px)-(vx*py))/p3_2,px/psqrt,py/psqrt;
+   
+  //std::cout << "Hj" << std::endl; std::cout << Hj << std::endl;
   return Hj;
+  
 }
 
