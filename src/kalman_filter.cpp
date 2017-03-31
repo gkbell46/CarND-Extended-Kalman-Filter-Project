dc1278cfa411fd99a566d2235a56191c1b1133de
gkbel46 <girish.bellenavar@gmail.com>
2017-03-31 23:54:17 +0530

    1. Grouped equations 2.Commented debugging messages

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -3,13 +3,8 @@
 
 
 MatrixXd I; // Identity matrix
-VectorXd u;	// external motion
 
-KalmanFilter::KalmanFilter() {
-  
-  u = VectorXd(2);
-  u << 0,0;
-}
+KalmanFilter::KalmanFilter() {}
 
 KalmanFilter::~KalmanFilter() {}
 
@@ -30,7 +25,7 @@ void KalmanFilter::Predict() {
     * predict the state
   */
   
-  x_ = F_ * x_ ;  
+  x_ = (F_ * x_)  ;  
   P_ = (F_ * P_ * F_.transpose()) + Q_;
   //std::cout << "x_:" << std::endl; std::cout << x_ << std::endl;
   //std::cout << "P_:" << std::endl; std::cout << P_ << std::endl;
@@ -42,9 +37,9 @@ void KalmanFilter::Update(const VectorXd &z) {
     * update the state by using Kalman Filter equations
   */
   
-  MatrixXd y = z - H_ * x_;
+  MatrixXd y = z - (H_ * x_);
   //std::cout << "y:" << std::endl; std::cout << y << std::endl;
-  MatrixXd S = H_ * P_ * H_.transpose() + R_;
+  MatrixXd S =  (H_ * P_ * H_.transpose())  + R_;
   //std::cout << "S:" << std::endl; std::cout << S << std::endl;
   MatrixXd k = P_ * H_.transpose() * S.inverse();
   //std::cout << "k:" << std::endl; std::cout << k << std::endl;
@@ -54,7 +49,7 @@ void KalmanFilter::Update(const VectorXd &z) {
   //std::cout << "I:" << std::endl; std::cout << I << std::endl;
   // new state
   x_ = x_ + (k * y);
-  std::cout << "x_:" << std::endl; std::cout << x_ << std::endl;
+  //std::cout << "x_:" << std::endl; std::cout << x_ << std::endl;
   P_ = (I - (k * H_)) * P_;
   //std::cout << "P_:" << std::endl; std::cout << P_ << std::endl;
   
@@ -73,14 +68,14 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   //std::cout << "z(0)" << std::endl; std::cout << x_ << std::endl;
   float hx_1  = sqrt((px * px) + (py * py));
   float hx_2 = atan2(py,px);
-  float hx_3 = (px * vx + py * vy) / hx_1; 
+  float hx_3 = ((px * vx) + (py * vy)) / hx_1; 
   VectorXd hx_ = VectorXd(3);
   hx_ << hx_1, 	
   	 hx_2, 
   	 hx_3;
   
   MatrixXd y = z - hx_;
-  MatrixXd S = H_ * P_ * H_.transpose() + R_;
+  MatrixXd S = (H_ * P_ * H_.transpose()) + R_;
   MatrixXd k = P_ * H_.transpose() * S.inverse();
   I = MatrixXd::Identity(x_.size(),x_.size());
   //std::cout << "I:" << std::endl; std::cout << I << std::endl;
